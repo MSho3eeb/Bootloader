89a5122b2a6aa91401be83f5b2cdfdbc6a729649
Mohamed Shoaib <92427903+MSho3eeb@users.noreply.github.com>
2023-09-19 16:44:01 +0300

    UART bootloader, python script added

--- Debug/main.d
+++ Debug/main.d
@@ -16,8 +16,11 @@ main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
 main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
 main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h
 main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h
 main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
 main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/flash.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/uart.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h
 main.obj: ../tm4c123gh6pm.h
 
 ../main.c:
@@ -52,9 +55,15 @@ C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h:
 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
 
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h:
+
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
 
 C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/flash.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/uart.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h:
 
 ../tm4c123gh6pm.h:
 
---
+++ PCloader.py
@@ -0,0 +1,46 @@
+import serial
+import time
+
+serial_port = 'COM5'
+baud_rate = 115200
+
+try:
+    ser = serial.Serial(serial_port, baud_rate);
+    print("Connected")
+    
+except serial.SerialException as e:
+    print(f"Failed {e}")
+    exit()
+    
+
+file_path = 'E:\\C_EUI\\BOOOOOTLOAADDEEERR\\examples\\test.hex' #path
+
+time.sleep(1)
+
+try:
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    for line in lines:
+        line = line.strip()
+        val = line[:4]
+        
+        if line:
+            
+            data_to_send = int(val, 16).to_bytes(1, byteorder='little')
+            ser.write(data_to_send)
+            #print(data_to_send)
+            print(f"***********************/// {line} sent ///***********************")
+            time.sleep(0.03)
+            
+    data_to_send = int("0xaa", 16).to_bytes(1, byteorder='little')
+    ser.write(data_to_send)
+
+except FileNotFoundError:
+    print("File not Found")
+    
+except Exception as e:
+    print(f"error: {e}")
+    
+ser.close()
+        
